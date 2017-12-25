563850728e76009746c105ad14ffa4b7828b7262
mpakunderscore <mpakunderscore@gmail.com>
2017-12-25 09:52:17 +0300

    React App example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render('Test', document.getElementById('root'));
 // registerServiceWorker();
 
-ReactDOM.render('Hello, world!', document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));

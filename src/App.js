13b31e4516225bd29e3e8e8322ca9a5a8c737023
mpakunderscore <mpakunderscore@gmail.com>
2017-12-25 10:05:08 +0300

    React App with AJAX

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    componentDidMount() {
+
+        axios.get(`https://cms-dot.herokuapp.com/api/posts`).then(res => {
+
+                const posts = res.data.data.children.map(obj => obj.data);
+                this.setState({ posts });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.posts.map(post =>
+                        <li key={post.id}>{post.title}</li>
+                    )}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;

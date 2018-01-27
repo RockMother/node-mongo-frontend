d70f5d6c62a8f8eeca760fbb59d771ce4a94a99e
mpakunderscore <mpakunderscore@gmail.com>
2018-01-28 01:00:49 +0300

    Fix styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import './styles/styles';
-import 'font-awesome/css/font-awesome.css';
+
 import App from './components/app';
 
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
-
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,2 +1,3 @@
 import './../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
 import './index.css';

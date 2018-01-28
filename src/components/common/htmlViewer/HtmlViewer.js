afb021f8445e9d730524bb36488b541fc8a0d520
mpakunderscore <mpakunderscore@gmail.com>
2018-01-28 21:57:27 +0300

    Remove old code.

--- src/components/common/htmlViewer/HtmlViewer.js
+++
@@ -1,7 +0,0 @@
-import CodeMirror from 'react-codemirror';
-import './../../../../node_modules/codemirror/mode/xml/xml';
-import './../../../../node_modules/codemirror/lib/codemirror.css';
-import './HtmlViewer.css';
-
-export default class HtmlViewer extends CodeMirror {
-}

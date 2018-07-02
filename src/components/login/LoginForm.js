9f5aebc6cfe0ea5f3f6c62e6c49f8c73f7d595b8
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-07-02 05:56:16 +0300

    LoginForm back

---
+++ src/components/login/LoginForm.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+import { bindToThis } from './../../../utils/utils'
+
+export default class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        bindToThis(this, this.loginClicked);
+    }
+
+    loginClicked(){
+        alert('Login clicked');
+    }
+
+    render() {
+        return (
+            <div className="login-form">
+                <div className="title">Login</div>
+                <form className="form vertical">
+                    <input type="text" placeholder="Username or email"/>
+                    <input type="password" placeholder="Password"/>
+                    <input  className="login-button" type="submit" value="Log in" onClick={this.loginClicked}/>
+                </form>
+                <div className="additional-links">
+                    <input type="button" value="Register"/>
+                    <div className="separator">/</div>
+                    <input type="button" value="Forgot password"/>
+                </div>
+
+            </div>
+        );
+    }
+}

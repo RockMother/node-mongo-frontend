3858cdd024670811b15e0bcab4d02160a53e7140
Pavel Kuzmin <mpakunderscore@gmail.com>
2018-07-02 06:08:53 +0300

    LoginForm back #2

--- src/components/app.js
+++ src/components/app.js
@@ -5,27 +5,17 @@ import { connect } from 'react-redux';
 import Menu from './menu/Menu';
 import PostsContainer from './posts/PostsContainer';
 import TemplatesContainer from './templates/TemplatesContainer';
-import Login from './settings/Login';
-import Settings from './settings/Settings';
+import LoginForm from './login/loginForm/LoginForm';
 
 class App extends Component {
     render() {
-
-        // let system = [];
-        // system.push(<Route key={states.length} path="/login" component={() => <Login/>} />);
-        // system.push(<Route key='settings' path="/settings" component={() => <Settings/>} />);
-
-        let states = this.props.categories.map((category, index) => {
+        const states = this.props.categories.map((category, index) => {
             return <Route exact
                 key={index}
                 path={`/${category.toLowerCase()}`}
-                component={() => category === 'Templates' || category === 'Settings' ?
-                    (category === 'Templates' ? <TemplatesContainer/> : <Settings/>) : <PostsContainer category={category} />}
-            />
+                component={() => category === 'Templates'? <TemplatesContainer/> : <PostsContainer category={category} />} />
         });
-
-        // const states = [].push.apply(system, category);
-
+        states.push(<Route key={states.length} path="/login" component={() => <LoginForm/>} />);
         return (
             <main>
                 <Menu />
